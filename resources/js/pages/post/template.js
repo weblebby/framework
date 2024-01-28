1340c45352d3be6b3eee9845e3ead20c7ca34bc4
Davut Kember <davutkmbr@gmail.com>
2024-01-28 13:33:20 +0300

    check template select is exist

--- resources/js/pages/post/template.js
+++ resources/js/pages/post/template.js
@@ -2,50 +2,54 @@ import api from '../../_api.js'
 import { Tab } from '../../_tab.js'
 import ConditionalField from '../../_conditional-field.js'
 
-const templateSelect = document.getElementById('template')
-const templateOptions = templateSelect.querySelectorAll('option')
+;(() => {
+    const templateSelect = document.getElementById('template')
+    if (!templateSelect) return
 
-const fetchPostFields = async (postType, template) => {
-    const url = window.Feadmin.Theme.postFieldsUrl
-        .replace(':theme', '__ACTIVE')
-        .replace(':template', template)
+    const templateOptions = templateSelect.querySelectorAll('option')
 
-    const params = new URLSearchParams({
-        type: postType,
-    })
+    const fetchPostFields = async (postType, template) => {
+        const url = window.Feadmin.Theme.postFieldsUrl
+            .replace(':theme', '__ACTIVE')
+            .replace(':template', template)
 
-    return await api(`${url}?${params}`)
-}
-
-const handleOnChange = () => {
-    const selectedOption = templateOptions[templateSelect.selectedIndex]
-
-    document.querySelectorAll('[data-template-tab]').forEach(node => {
-        node.remove()
-    })
-
-    const setTabs = async () => {
-        if (!selectedOption.value) return
-
-        const response = await fetchPostFields(
-            templateSelect.dataset.postType,
-            selectedOption.value,
-        )
-
-        response.tabs.forEach(tab => {
-            const createdTab = Tab.create({
-                container: 'post',
-                id: tab.id,
-                title: tab.title,
-                content: tab.fields,
-            })
-
-            ConditionalField.listen(createdTab.content)
+        const params = new URLSearchParams({
+            type: postType,
         })
+
+        return await api(`${url}?${params}`)
     }
 
-    setTabs()
-}
+    const handleOnChange = () => {
+        const selectedOption = templateOptions[templateSelect.selectedIndex]
 
-templateSelect.addEventListener('change', handleOnChange)
-handleOnChange()
+        document.querySelectorAll('[data-template-tab]').forEach(node => {
+            node.remove()
+        })
+
+        const setTabs = async () => {
+            if (!selectedOption.value) return
+
+            const response = await fetchPostFields(
+                templateSelect.dataset.postType,
+                selectedOption.value,
+            )
+
+            response.tabs.forEach(tab => {
+                const createdTab = Tab.create({
+                    container: 'post',
+                    id: tab.id,
+                    title: tab.title,
+                    content: tab.fields,
+                })
+
+                ConditionalField.listen(createdTab.content)
+            })
+        }
+
+        setTabs()
+    }
+
+    templateSelect.addEventListener('change', handleOnChange)
+    handleOnChange()
+})()
