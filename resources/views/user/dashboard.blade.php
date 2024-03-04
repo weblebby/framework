8745782a8ea8a588462535a52b73fc1b6d76d074
Davut Kember <davutkmbr@gmail.com>
2024-03-04 13:25:10 +0300

    remove visitor sections

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -5,8 +5,7 @@
             <x-weblebby::page.subtitle>@lang('Dear :name, welcome to the administration panel.', ['name' => auth()->user()->first_name])</x-weblebby::page.subtitle>
         </x-weblebby::page.head>
         <div class="fd-grid fd-gap-3 md:fd-grid-cols-2 lg:fd-grid-cols-3 xl:fd-grid-cols-4">
-            <x-ext-visitors::user.current-month-visitors-chart />
-            <x-ext-visitors::user.online-visitors-chart />
+            ...
         </div>
     </x-weblebby::page>
 </x-weblebby::layouts.panel>
